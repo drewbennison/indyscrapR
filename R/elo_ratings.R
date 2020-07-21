72eb4d6368feb87caffce2d5a88215f0717d99f0
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-07-20 22:12:21 -0400

    Add files via upload

---
+++ R/elo_ratings.R
@@ -0,0 +1,20 @@
+#' Load IndyCar Elo Ratings
+#'
+#' This function loads in the current IndyCar Elo ratings.
+#' @export
+
+current_elo_ratings <- function() {
+  elo_ratings_file <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/elo_ratings/elo_tracker.csv")
+  elo_ratings <- elo_ratings_file %>% filter(year>2000) %>%
+    mutate(date=ymd(date)) %>%
+    group_by(driver) %>%
+    slice(which.max(as.Date(date, '%m/%d/%Y'))) %>%
+    mutate(EloRating = round(EloRating),
+           PreviousEloRating = round(PreviousEloRating)) %>%
+    arrange(-EloRating) %>%
+    select(-year) %>%
+    select(-PreviousEloRating) %>%
+    rename(Driver = driver) %>%
+    rename(LastUpdated = date)
+  return(elo_ratings)
+}
