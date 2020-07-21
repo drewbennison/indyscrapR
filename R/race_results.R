6b5fd933512dc4f58effa69610fa32c6e697f4ea
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-07-21 11:09:02 -0400

    Add files via upload

--- R/race_results.R
+++ R/race_results.R
@@ -7,7 +7,7 @@ race_results <- function(year=2020, race_number=1) {
   #can use advanced stats
   if(year>2018){
     dt2 <- dt %>% filter(year==year, raceNumber==race_number) %>%
-      select(-ledPts, -ledMostPoints, -polePoints, -xPtsATP25, -xPts)
+      select(-ledPts, -ledMostPts, -polePoints, -xPtsATP25, -xPts)
     return(dt2)
   } else {
     dt2 <- dt
