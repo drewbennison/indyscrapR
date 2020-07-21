f5ef17dc3251d056b0ca7e56d1b57217daa36545
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-07-21 17:20:05 -0400

    Add files via upload

---
+++ R/season_stats.R
@@ -0,0 +1,122 @@
+#' Calculate IndyCar Full Season Stats
+#'
+#' This function calculates full season basic and advanced IndyCar stats.
+#' @export
+season_stats <- function(season=2020, track_type="all") {
+  dt <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/master_backup/indycar_results.csv")
+
+  if(track_type=="all") {
+  avgPE <- dt %>% filter(!is.na(passesFor)) %>%
+    select(driver, st, passesFor, passesAgainst) %>%
+    mutate(passEff = passesFor/(passesFor+passesAgainst),
+           passEff = ifelse(is.na(passEff),.5,passEff)) %>%
+    group_by(st) %>%
+    summarise(avgPE = mean(passEff)) %>%
+    select(st, avgPE)
+
+  #Calculate AFP from every starting position
+  afp <- dt %>%
+    group_by(st) %>%
+    summarise(xFP = mean(fin))
+  season<- 2020
+  dt <- dt %>%
+    filter(year==season) %>%
+    left_join(afp, by=c("st" = "st")) %>%
+    mutate(xFPDifference=xFP-fin)
+
+  dt <- dt %>%
+    mutate(passEff = passesFor/(passesFor+passesAgainst),
+           passEff = ifelse(is.na(passEff), .5, passEff)) %>%
+    left_join(avgPE, by="st") %>%
+    mutate(AdjPassEff = passEff-avgPE)
+
+  driver_season_stats <- dt %>%
+    group_by(driver) %>%
+    mutate(percFavorablePass = 100*(sum(passesFor)/(sum(passesFor)+sum(passesAgainst))),
+           favorableStart = ifelse(lapOneChange>=0, 1,
+                                   ifelse(lapOneChange<0, 0, NA)),
+           StartRetention = 100*mean(favorableStart),
+           StartPM = sum(lapOneChange),
+           Races = n(),
+           PMperStart = StartPM/Races,
+           Pts=sum(pts),
+           xPoints = sum(xPts),
+           AFP = mean(fin),
+           DevFP = sd(fin),
+           ASP = mean(st),
+           DevSP = sd(st),
+           ATP = mean(atp),
+           DevATP = sd(atp),
+           ATP25 = mean(atp25),
+           DevATP25 = sd(atp25),
+           PassEff = 100*mean(passEff),
+           AdjPassEff = 100*mean(AdjPassEff),
+           RunningCheck = ifelse(status=="running",1,0),
+           RunPerc = 100*mean(RunningCheck),
+           AFS = mean(fastLapRank),
+           Top5Perc = 100*(sum(inTopFive)/sum(laps)),
+           #Average Surplus Position
+           AEP = mean(xFPDifference)) %>%
+    #SELECT DRIVER AND ANY VARIBLES BEFORE YOU SELECT DISTINCT
+    distinct(driver, StartRetention, StartPM, Races, PMperStart, Pts, xPoints, AFP, DevFP, ASP, DevSP, ATP, DevATP, ATP25, DevATP25, PassEff, AdjPassEff, RunPerc, Top5Perc, AEP, AFS) %>%
+    select(driver, Races, Pts, xPoints, AFP, DevFP, ASP, DevSP, ATP, DevATP, ATP25, DevATP25, PassEff, AdjPassEff, RunPerc, Top5Perc, AEP, AFS, StartRetention, StartPM, PMperStart)
+  }
+
+  else{
+    avgPE <- dt %>% filter(!is.na(passesFor)) %>%
+      select(driver, st, passesFor, passesAgainst) %>%
+      mutate(passEff = passesFor/(passesFor+passesAgainst),
+             passEff = ifelse(is.na(passEff),.5,passEff)) %>%
+      group_by(st) %>%
+      summarise(avgPE = mean(passEff)) %>%
+      select(st, avgPE)
+
+    #Calculate AFP from every starting position
+    afp <- dt %>%
+      group_by(st) %>%
+      summarise(xFP = mean(fin))
+
+    dt <- dt %>%
+      filter(year==season, type==track_type) %>%
+      left_join(afp, by=c("st" = "st")) %>%
+      mutate(xFPDifference=xFP-fin)
+
+    dt <- dt %>%
+      mutate(passEff = passesFor/(passesFor+passesAgainst),
+             passEff = ifelse(is.na(passEff), .5, passEff)) %>%
+      left_join(avgPE, by="st") %>%
+      mutate(AdjPassEff = passEff-avgPE)
+
+    driver_season_stats <- dt %>%
+      group_by(driver) %>%
+      mutate(percFavorablePass = 100*(sum(passesFor)/(sum(passesFor)+sum(passesAgainst))),
+             favorableStart = ifelse(lapOneChange>=0, 1,
+                                     ifelse(lapOneChange<0, 0, NA)),
+             StartRetention = 100*mean(favorableStart),
+             StartPM = sum(lapOneChange),
+             Races = n(),
+             PMperStart = StartPM/Races,
+             Pts=sum(pts),
+             xPoints = sum(xPts),
+             AFP = mean(fin),
+             DevFP = sd(fin),
+             ASP = mean(st),
+             DevSP = sd(st),
+             ATP = mean(atp),
+             DevATP = sd(atp),
+             ATP25 = mean(atp25),
+             DevATP25 = sd(atp25),
+             PassEff = 100*mean(passEff),
+             AdjPassEff = 100*mean(AdjPassEff),
+             RunningCheck = ifelse(status=="running",1,0),
+             RunPerc = 100*mean(RunningCheck),
+             AFS = mean(fastLapRank),
+             Top5Perc = 100*(sum(inTopFive)/sum(laps)),
+             #Average Surplus Position
+             AEP = mean(xFPDifference)) %>%
+      #SELECT DRIVER AND ANY VARIBLES BEFORE YOU SELECT DISTINCT
+      distinct(driver, StartRetention, StartPM, Races, PMperStart, Pts, xPoints, AFP, DevFP, ASP, DevSP, ATP, DevATP, ATP25, DevATP25, PassEff, AdjPassEff, RunPerc, Top5Perc, AEP, AFS) %>%
+      select(driver, Races, Pts, xPoints, AFP, DevFP, ASP, DevSP, ATP, DevATP, ATP25, DevATP25, PassEff, AdjPassEff, RunPerc, Top5Perc, AEP, AFS, StartRetention, StartPM, PMperStart)
+
+  }
+}
