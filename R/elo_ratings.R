5a049ff9a5c4cb97a4c36a52f42e8f04962a74d2
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-07-24 12:03:26 -0400

    Add files via upload

--- R/elo_ratings.R
+++ R/elo_ratings.R
@@ -1,18 +1,20 @@
 #' Load IndyCar Elo Ratings
 #'
 #' This function loads in the current IndyCar Elo ratings.
+#'
+#' @importFrom magrittr "%>%"
 #' @export
 current_elo_ratings <- function() {
   elo_ratings_file <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/elo_ratings/elo_tracker.csv")
-  elo_ratings <- elo_ratings_file %>% filter(year>2000) %>%
-    mutate(date=ymd(date)) %>%
-    group_by(driver) %>%
-    slice(which.max(as.Date(date, '%m/%d/%Y'))) %>%
-    mutate(EloRating = round(EloRating),
+  elo_ratings <- elo_ratings_file %>% dplyr::filter(year>2000) %>%
+    dplyr::mutate(date=ymd(date)) %>%
+    dplyr::group_by(driver) %>%
+    dplyr::slice(dplyr::which.max(as.Date(date, '%m/%d/%Y'))) %>%
+    dplyr::mutate(EloRating = round(EloRating),
            PreviousEloRating = round(PreviousEloRating)) %>%
-    arrange(-EloRating) %>%
-    select(-year) %>%
-    select(-PreviousEloRating)
+    dplyr::arrange(-EloRating) %>%
+    dplyr::select(-year) %>%
+    dplyr::select(-PreviousEloRating)
   return(elo_ratings)
 }
 
@@ -22,8 +24,8 @@ current_elo_ratings <- function() {
 #' @export
 historical_elo_ratings <- function() {
 elo_ratings_file <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/elo_ratings/elo_tracker.csv")
-elo_ratings <- elo_ratings_file %>% filter(year>2000) %>%
-  select(driver, date, year, EloRating) %>%
-  rename(season = year)
+elo_ratings <- elo_ratings_file %>% dplyr::filter(year>2000) %>%
+  dplyr::select(driver, date, year, EloRating) %>%
+  dplyr::rename(season = year)
 return(elo_ratings)
 }
