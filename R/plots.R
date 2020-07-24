a666f84bad5ef163cc4bb85f1073a5a1748c169a
DrewBennison <dhb5140@psu.edu>
2020-07-24 13:33:23 -0400

    select drivers

--- R/plots.R
+++ R/plots.R
@@ -4,10 +4,14 @@
 #' @importFrom magrittr "%>%"
 #' @export
 
-plot_championship_points <- function(season=2020){
+plot_championship_points <- function(season=2020, drivers){
+  if(is.na(drivers)){
+    warning("No drivers entered. Please set drivers=c('Driver1', 'Driver')")
+  }
+
   dt <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/master_backup/indycar_results.csv")
 
-  dt2 <- dt %>% dplyr::filter(year==season) %>%
+  dt2 <- dt %>% dplyr::filter(year==season, driver %in% drivers) %>%
     dplyr::select(driver, raceNumber, pts) %>%
     dplyr::group_by(driver) %>%
     dplyr::arrange(raceNumber) %>%
