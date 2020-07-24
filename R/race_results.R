a93becaabc6b5fd164f89cac0dd3f92382d448bc
drewbennison <46604140+drewbennison@users.noreply.github.com>
2020-07-24 11:27:01 -0400

    Add files via upload

--- R/race_results.R
+++ R/race_results.R
@@ -6,12 +6,12 @@ race_results <- function(season=2020, race_number=0) {
   dt <- read.csv("https://raw.githubusercontent.com/drewbennison/thesingleseater/master/datasets/master_backup/indycar_results.csv")
   #return full season stats
   if(race_number !=0){
-    dt2 <- dplyr::filter(dt, year==season, raceNumber==race_number)
-    dt2 <- dplyr::select(dt, -xPtsATP25, -xPtsATP)
+    dt2 <- dt %>% dplyr::filter(year==season, raceNumber==race_number) %>%
+      dplyr::select(-xPtsATP25, -xPtsATP)
     return(dt2)
   } else {
-    dt2 <- dplyr::filter(dt, year==season)
-    dt2 <- dplyr::select(dt2, -xPtsATP25, -xPtsATP)
+    dt2 <- dt %>% dplyr::filter(year==season) %>%
+      dplyr::select(-xPtsATP25, -xPtsATP)
     return(dt2)
   }
   }
